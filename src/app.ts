173308a81e3a7160df2307a2ceee36762ab607c8
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-10-02 13:07:23 +0600

    bikehub client link add

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,12 @@ import { paymentRouter } from "./app/modules/payment/payment.route";
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://bikehub-client.netlify.app"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/api", router);
